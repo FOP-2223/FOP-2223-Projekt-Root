2528b48f10967fff93be0f4529c6dcff75b0eaa7
Daniel Mangold <mangold.daniel@protonmail.com>
2022-02-27 19:04:35 +0100

    oops

--- src/main/java/projekt/Main.java
+++ src/main/java/projekt/Main.java
@@ -3,6 +3,8 @@ package projekt;
 import projekt.base.EuclideanDistanceCalculator;
 import projekt.base.Location;
 import projekt.delivery.DeliveryService;
+import projekt.delivery.Simulation;
+import projekt.delivery.SimulationConfig;
 import projekt.delivery.routing.Region;
 import projekt.delivery.routing.VehicleManager;
 import projekt.gui.MainFrame;
@@ -21,36 +23,43 @@ public class Main {
 
         // // layer 1
 
-        // Region region = Region.builder()
-        //     .addNeighborhood("nodeA", new Location(-2, -2), 1.5)
-        //     .addNode("nodeB", new Location(-2, 2))
-        //     .addNeighborhood("nodeC", new Location(2, 2), 0.5)
-        //     .addNode("nodeD", new Location(2, -2))
-        //     .addEdge("edge1", new Location(-2, -2), new Location(-2, 2), Duration.ofMinutes(1))
-        //     .addEdge("edge2", new Location(-2, 2), new Location(-2, 2), Duration.ofMinutes(1))
-        //     .addEdge("edge3", new Location(-2, -2), new Location(-2, 2), Duration.ofMinutes(1))
-        //     .addEdge("edge4", new Location(-2, -2), new Location(-2, 2), Duration.ofMinutes(1))
-        //     .build();
+         Region region = Region.builder()
+             .addNode("origin", new Location(0, 0))
+             .addNeighborhood("nodeA", new Location(-2, -2), 1.5)
+             .addNode("nodeB", new Location(-2, 2))
+             .addNeighborhood("nodeC", new Location(2, 2), 0.5)
+             .addNode("nodeD", new Location(20, -20))
+             .addEdge("edge1", new Location(0, 0), new Location(20, -20), Duration.ofMinutes(1))
+             .addEdge("edge2", new Location(0, 0), new Location(2, 2), Duration.ofMinutes(1))
+//             .addEdge("edge2", new Location(-2, 2), new Location(-2, 2), Duration.ofMinutes(1))
+//             .addEdge("edge3", new Location(-2, -2), new Location(-2, 2), Duration.ofMinutes(1))
+//             .addEdge("edge4", new Location(-2, -2), new Location(-2, 2), Duration.ofMinutes(1))
+             .build();
 
         // // layer 2
 
-        // VehicleManager vehicleManager = VehicleManager.SIMPLE.create(region, new EuclideanDistanceCalculator());
+         VehicleManager vehicleManager = VehicleManager.SIMPLE.create(region, new EuclideanDistanceCalculator());
 
         // // layer 3
 
-        // DeliveryService deliveryService = DeliveryService.SIMPLE.create(vehicleManager, new LinearRater());
+         DeliveryService deliveryService = DeliveryService.SIMPLE.create(vehicleManager, new LinearRater(), new Simulation() {
+             @Override
+             public void onStateUpdated() {
+
+             }
+         }, new SimulationConfig(50));
 
         // // layer 4
 
-        // Pizzeria pizzeria = Pizzeria.LOS_FOPBOTS_HERMANOS.create(deliveryService);
+         Pizzeria pizzeria = Pizzeria.LOS_FOPBOTS_HERMANOS.create(deliveryService);
 
         // the lasagna is complete
 
         // Gui Setup
         FlatDarkLaf.setup();
         SwingUtilities.invokeLater(()->{
-            new MainFrame(null, null, null, null).setVisible(true); // -> starts GUI thread
-            // new MainFrame(region, vehicleManager, deliveryService, pizzeria); // -> starts GUI thread
+//            new MainFrame(null, null, null, null).setVisible(true); // -> starts GUI thread
+             new MainFrame(region, vehicleManager, deliveryService, pizzeria).setVisible(true); // -> starts GUI thread
 
         });
 
