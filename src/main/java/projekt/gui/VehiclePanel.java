649b1c7c1beb9433a08fef9a43bbbb7f52fef4ae
Dustin Glaser <dustin.glaser@posteo.de>
2022-03-03 00:43:59 +0100

    replace JComboBox by JTable

--- src/main/java/projekt/gui/VehiclePanel.java
+++ src/main/java/projekt/gui/VehiclePanel.java
@@ -5,14 +5,13 @@ import projekt.delivery.routing.Vehicle;
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
-import java.util.*;
+import java.util.Objects;
 
 public class VehiclePanel extends JPanel {
 
     private final MainFrame mainFrame;
 
-    private JScrollPane scrollPane;
-    private JComboBox<Vehicle> selectionArea;
+    private JList<Vehicle> selectionList;
 
     public VehiclePanel(MainFrame mainFrame) {
         this.mainFrame = mainFrame;
@@ -21,74 +20,45 @@ public class VehiclePanel extends JPanel {
 
     public void initComponents() {
         setLayout(new GridLayout(0, 1));
-        setBorder(new TitledBorder("Location Details"));
-        selectionArea = new JComboBox<>();
+        setBorder(new TitledBorder("Vehicle Selection"));
+        selectionList = new JList<>();
+        var scrollPane = new JScrollPane(selectionList);
+        add(scrollPane, BorderLayout.CENTER);
+        selectionList.setCellRenderer(new ListCellRenderer<>() {
 
-        add(selectionArea, BorderLayout.CENTER);
-        // TODO: Basically everything lol
-        // TODO: Replace with JTable, Add Data
-
-        selectionArea.setRenderer(new ListCellRenderer<Vehicle>() {
-
-            ListCellRenderer<? super Vehicle> original = selectionArea.getRenderer();
+            final ListCellRenderer<? super Vehicle> original = selectionList.getCellRenderer();
 
             @Override
             public Component getListCellRendererComponent(JList<? extends Vehicle> list, Vehicle value, int index, boolean isSelected, boolean cellHasFocus) {
-
                 var component = (JLabel) original.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-                if (value != null) {
-                    component.setText(String.format("Vehicle %d", value.getId()));
-                } else {
-                    component.setText("No Vehicle");
-                }
+                component.setText(String.format("Vehicle %d", value.getId()));
                 return component;
             }
         });
+        selectionList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        selectionList.setModel(new AbstractListModel<>() {
 
-        selectionArea.addActionListener(l -> {
-            mainFrame.setSelectedVehicle((Vehicle) selectionArea.getSelectedItem());
+            Vehicle[] vehicles = null;
+
+            @Override
+            public int getSize() {
+                return mainFrame.getVehicleManager().getVehicles().size();
+            }
+
+            @Override
+            public Vehicle getElementAt(int index) {
+                return (vehicles = Objects.requireNonNullElseGet(vehicles, () -> mainFrame.getVehicleManager().getVehicles().toArray(Vehicle[]::new)))[index];
+            }
         });
-
+        selectionList.addListSelectionListener(l -> mainFrame.setSelectedVehicle(selectionList.getSelectedValue()));
     }
 
-    public void setVehicles(Collection<Vehicle> vehicles) {
-        if (selectionArea.getItemCount() > 0)
-            // initialize selection are only once
-            return;
-        selectionArea.addItem(null);
-        vehicles.forEach(selectionArea::addItem);
-
+    public void onUpdate() {
+        SwingUtilities.updateComponentTreeUI(selectionList);
     }
 
     public void setSelectedVehicle(Vehicle vehicle) {
-        selectionArea.setSelectedItem(vehicle);
+        selectionList.setSelectedValue(vehicle, true);
     }
-
-//    private static class VehicleEntry {
-//
-//        private static final VehicleEntry NULL = new VehicleEntry(null);
-//
-//        private final Vehicle vehicle;
-//
-//        private VehicleEntry(@Nullable Vehicle vehicle) {
-//            this.vehicle = vehicle;
-//        }
-//
-//
-//
-//        @Override
-//        public String toString() {
-//            if (vehicle == null)
-//                return "No Vehicle";
-//            return String.format("Vehicle %d", vehicle.getId());
-//
-//        }
-//
-//        @Override
-//        public boolean equals(Object obj) {
-//            return super.equals(obj) || Objects.equals(vehicle, obj);
-//        }
-//
-//    }
 
 }
