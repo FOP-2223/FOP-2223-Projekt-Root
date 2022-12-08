473ca9785229caecbe5403f8a5a0740ed8896d0d
LukasKlenner <Klenner-lukas@t-online.de>
2022-12-09 00:27:10 +0100

    make JUnit Tests executable

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,3 +1,15 @@
 dependencies {
     implementation(libs.annotations)
+    testImplementation(libs.junit.core)
+}
+
+tasks {
+    test {
+        val runDir = File("../build/run")
+        doFirst {
+            runDir.mkdirs()
+        }
+        workingDir = runDir
+        useJUnitPlatform()
+    }
 }
